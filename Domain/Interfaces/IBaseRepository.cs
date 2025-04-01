3a9c2525715267841123ad85af739f04acdcb10a
JoaquinGuerrero25 <joaquinguerrero256@gmail.com>
2025-04-01 19:00:23 -0300

    Dependencias, IRepository en domain y relacion entre proyectos.

---
+++ Domain/Interfaces/IBaseRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Interfaces
+{
+    public interface IBaseRepository<T> where T : class
+    {
+        T GetById(string id);
+        IEnumerable<T> GetAll();
+        T Create(T entity);
+        T Update(T entity);
+        T Delete(string id);
+    }
+}
